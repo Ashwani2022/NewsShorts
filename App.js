8c9aaf62d4303a50cc0fc7066d06064c0ecd8978
Ashwani2022 <90569780+Ashwani2022@users.noreply.github.com>
2022-05-21 01:48:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,30 @@
+import React,{ useContext} from 'react';
+import { StyleSheet, Text, View,StatusBar } from 'react-native';
+import Context, { NewsContext } from './API/Context';
+import InShortTabs from './components/InShortTabs';
+
+
+ function App() {
+
+  const {darkTheme}=useContext(NewsContext)
+
+  return (
+    //  colour  can be changed from here
+    <View style={{...styles.container,backgroundColor: darkTheme ?'#282C35' : "white"}}>    
+      <InShortTabs/>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight,
+  },
+});
+
+export default ()=>{
+  return (
+  <Context>
+    <App/>
+  </Context>) ;
+} 
